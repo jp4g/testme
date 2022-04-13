6d8b22c4a34034be888db57452b06c7984059113
Jack Gilcrest <me@jp4g.llc>
2022-04-13 17:33:03 -0400

    alert

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const query = async () => {
   const url = 'https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/degods?edge_cache=true';
   const data = await fetch(url);
-  console.log('data: ', await data.json())
+  alert(JSON.stringify((await data.json())))
 }
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={query()} />
+        <button onClick={query()} style={{width: 200, height: 100}} />
         <a
           className="App-link"
           href="https://reactjs.org"
